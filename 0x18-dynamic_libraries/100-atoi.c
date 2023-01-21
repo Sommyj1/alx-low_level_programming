5edfce7dc679799eedcaf9262e75144d96ffa64c
Sommyj1 <joseph2blessing2015@gmail.com>
2023-01-20 17:22:22 -0800

    Task 2

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,48 +1,139 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- *_atoi - Entry point
- *@s: pointer to the string
- *Return: no return
+ * _pow - Computes the power of a number by another.
+ * @a: The first number.
+ * @n: The power to compute.
+ * Return: a ^ n.
+ */
+int _pow(int a, int n)
+{
+	int i, res = a;
+
+	if (n == 0)
+		res = 1;
+	else if (n > 1)
+		for (i = 1; i < n; i++)
+			res = res * a;
+	return (res);
+}
+
+
+/**
+ * char_to_int - Converts a char digit to its integer value.
+ * @c: The ascii character.
+ * Return: The integer value of c.
+ */
+int char_to_int(char c)
+{
+	int res;
+
+	switch ((int)c)
+	{
+	case 48:
+		res =  0;
+		break;
+	case 49:
+		res =  1;
+		break;
+	case 50:
+		res =  2;
+		break;
+	case 51:
+		res =  3;
+		break;
+	case 52:
+		res =  4;
+		break;
+	case 53:
+		res =  5;
+		break;
+	case 54:
+		res =  6;
+		break;
+	case 55:
+		res =  7;
+		break;
+	case 56:
+		res =  8;
+		break;
+	case 57:
+		res =  9;
+		break;
+	default:
+		res = 0;
+	}
+	return (res);
+}
+
+
+
+
+
+
+/**
+ * order - Returns the order of an integer contained in a string.
+ * @s: The string whose the order should be returned.
+ * Return: The order of s.
+ */
+int order(char *s)
+{
+	int i = -1, o = 0, found_1st_int = 0;
+	char tmp;
+
+	do {
+		tmp = s[++i];
+		if (_isdigit(tmp))
+		{
+			o++;
+			found_1st_int = 1;
+		}
+		else if (found_1st_int)
+		{
+			break;
+		}
+	} while (tmp != '\0');
+	return (o);
+}
+
+/**
+ * _atoi - Conversts a string to an integer.
+ * @s: The string to convert
+ * Return: The integer value of the string.
  */
 int _atoi(char *s)
 {
-	int i, j;
-	int negativos;
-	unsigned int entero;
-	int auxiliar;
+	int ci, i = -1, n = 0, sign = 1, res = 0, found_1st_int = 0;
+	char tmp;
 
-	i = 0;
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	auxiliar = 10;
-	entero = 0;
-	negativos = 0;
-	while (j < i)
-	{
-		if (s[j] == '-')
-			negativos = negativos + 1;
-		if (s[j] >= '0' && s[j] <= '9')
+	n = order(s);
+	do {
+		tmp = s[++i];
+		if (tmp == '-')
 		{
-			if (s[j + 1] >= '0' && s[j + 1] <= '9')
-			{
-				entero = (s[j] - '0') + entero;
-				entero = auxiliar * entero;
-			}
-			else
-			{
-				entero = (s[j] - '0') + entero;
-			}
-			if (s[j + 1] < '0' || s[j + 1] > '9')
-				break;
+			sign = sign * -1;
+			found_1st_int = 1;
 		}
-		j++;
-	}
-	if (negativos % 2 != 0)
-		return ((entero) * -1);
-		else
-			return (entero);
+		else if (found_1st_int)
+		{
+			break;
+		}
+	} while (tmp != '\0');
+	found_1st_int = 0;
+	i = -1;
+	do {
+		tmp = s[++i];
+		if (_isdigit(tmp))
+		{
+			ci = char_to_int(tmp);
+			res += (sign * ci * _pow(10, --n));
+			found_1st_int = 1;
+		}
+		else if (found_1st_int)
+		{
+			break;
+		}
+	} while (tmp != '\0');
+	return (res);
 }
